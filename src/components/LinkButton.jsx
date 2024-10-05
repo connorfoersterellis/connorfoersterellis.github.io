53601e90b58272ba29ff34154fe90a6eb6d0892a
Connor Ellis <connor.f.ellis@gmail.com>
2024-10-05 14:34:35 -0400

    Update LinkButton.jsx

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Button, Typography, Box } from '@mui/material'
 
 const LinkButton = ({ to, text, textProps, buttonProps }) => {
+  const navigate = useNavigate();
+
+  const handleButtonClick = (to) => {
+    navigate(to || '/judgemental-car-quiz')
+  }
+
   return (
     <Box m={1}>
       <Button 
         variant="contained" 
-        onClick={() => window.location.href = to || '/judgemental-car-quiz'}
+        onClick={() => handleButtonClick(to)}
         {...buttonProps}
       >
         <Typography {...textProps}>
